05717e0633a144cc869e2426b75a13d9683c9fd0
Julio Girardi <juliogirardi@msn.com>
2023-05-28 20:42:32 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -51,7 +51,8 @@ function refreshTable() {
 function refreshMedia() {
   const mediaFinal = calcMedia();
 
-  document.getElementById("media-final-valor").innerHTML = mediaFinal;
+  document.getElementById("media-final-valor").innerHTML =
+    mediaFinal.toFixed(2);
   document.getElementById("media-final-resultado").innerHTML =
     mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
